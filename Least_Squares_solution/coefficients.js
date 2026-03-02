da5a4268eaa4ff7173c56e214fbad33bcbb652b6
zivd <ziad99kr@gmail.com>
2026-03-02 20:48:48 +0100

    functions to calculate teta1 and teta0

--- Least_Squares_solution/coefficients.js
+++ Least_Squares_solution/coefficients.js
@@ -16,3 +16,30 @@ async function getData(){
             })
             return data;
 }
+function EnsembleVariables(){     /// ∑X ∑Y
+   
+    var ensembelY  = 0 , ensembelX = 0 ;
+    data.forEach((variable) =>{
+        ensembelY += variable.y;
+        ensembelX += variable.x;
+    })
+    return { ensembelY  , ensembelX }
+}
+
+function MultiplicationVariables(){   // ∑XY
+    var xy = 0;
+    data.forEach((variY) =>{ xy += variY.x * variY.y});
+    return xy;
+}
+
+function Powertwovariables(){   // ∑X^2 
+    var x_carre = 0;
+    data.forEach((variY) =>{ x_carre += Math.pow( variY.x , 2) })
+    return x_carre;
+}
+
+function Number_observations(){  // n 
+      return data.length;
+}
+
+
