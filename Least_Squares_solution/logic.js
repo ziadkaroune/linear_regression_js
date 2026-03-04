b78bc3df48853a77a10404463e268125b0049c35
zivd <ziad99kr@gmail.com>
2026-03-04 14:48:53 +0100

    function to calculate teta1 for our global equation

--- Least_Squares_solution/logic.js
+++ Least_Squares_solution/logic.js
@@ -8,3 +8,18 @@ async function initi_coefficients(){
         coefficients = await linear_regression_data();
 }
 
+
+// Slope θ1  teta_1
+async function calculate_tetaOne(){
+    const { ensembelY , ensembelX , x_pow_two , xy ,  number_observations} = coefficients;
+
+    const denominator  =  ((number_observations * x_pow_two) - (Math.pow(ensembelX , 2)));
+    if(denominator === 0) return null;
+
+    var tetaOne = ((number_observations * xy) - ( ensembelX * ensembelY )) / denominator;
+     
+    return tetaOne;
+
+}
+
+
